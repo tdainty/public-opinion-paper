1141890c0976da91ff09c9b8603f1a1ba2065b70
Thomas Dainty <tdainty926@gmail.com>
2024-11-18 13:40:15 -0600

    getting R Markdown fully set-up

--- RMarkdown Set-Up.R
+++ RMarkdown Set-Up.R
@@ -9,3 +9,7 @@ if (!require(RStata)) install.packages("RStata"); library(RStata) # this will in
 stata("my_do_file.do", 
       stata.path = "/Applications/Stata/StataMP.app/Contents/MacOS/stata-mp", # yours probably differs: use the chooseStataBin() command on windows or linux machines; on Macs, right click on the Stata app, select "Show Package Contents", then see what's in the Contents/MacOS/ directory
       stata.version = 13)  # again, specify what _you_ have
+
+library(usethis)
+
+usethis::edit_r_environ()
